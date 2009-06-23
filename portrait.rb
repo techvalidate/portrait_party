460a22c0e9aca9c9e2978ea577e96cd226f10125
Jordan McKible <jordan@mckible.com>
2009-06-23 15:00:21 -0700

    Portrait class.

--- portrait.rb
+++ portrait.rb
@@ -1,11 +1,27 @@
 require 'rubygems'
 require 'httparty'
+require 'open-uri'
+require 'fileutils'
 
 class Portrait
   include HTTParty
-  base_uri 'localhost:3000'
-  basic_auth 'username', 'password'
+  format :xml
+
+  def self.url(url)
+    Portrait.base_uri url
+  end
+  
+  def self.authorize(username, password)
+    Portrait.basic_auth username, password
+  end
+
+  def self.capture(url, dir, filename)
+    raise 'Please provide a Portait endpoint' unless default_options[:base_uri]
+    raise 'Set username and password'         unless default_options[:basic_auth]
+    portrait = Portrait.post '/', :query=>{:url=>url}
+    image = open "#{default_options[:base_uri]}#{portrait['site']['image_url']}"
+    FileUtils.cp image.path, "#{dir}/#{filename}"
+  rescue Net::HTTPFatalError
+    raise 'Invalid url'
+  end
 end
- 
-puts Portrait.post '/', :query=>{:url=>'http://github.com'}
-#puts response.body, response.code, response.message, response.headers.inspect
